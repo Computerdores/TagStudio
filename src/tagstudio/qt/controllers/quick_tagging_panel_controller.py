f0b86a0f6c54ab477631535c69a5c76dc533ac2d
Jann Stute <jann.stute@protonmail.com>
2025-07-15 22:56:15 +0200

    feat: add file attribute and field container widgets

--- src/tagstudio/qt/controllers/quick_tagging_panel_controller.py
+++ src/tagstudio/qt/controllers/quick_tagging_panel_controller.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from tagstudio.core.library.alchemy.enums import BrowsingState
+from tagstudio.core.library.alchemy.library import Library
 from tagstudio.core.utils.types import unwrap
 from tagstudio.qt.views.panel_modal import PanelModal
 from tagstudio.qt.views.quick_tagging_panel_view import QuickTaggingPanelView
@@ -10,18 +11,20 @@ if TYPE_CHECKING:
 
 
 class QuickTaggingPanel(QuickTaggingPanelView):
+    __lib: Library
     __results: list[int]
     __index: int
 
     def __init__(self, driver: "QtDriver"):
         super().__init__(driver)
+        self.__lib = driver.lib
 
     def set_search(self, query: BrowsingState) -> None:
         self.__index = 0
 
-        self.__results = self._lib.search_library(query, None).ids
+        self.__results = self.__lib.search_library(query, None).ids
 
-        self._set_entry(unwrap(self._lib.get_entry(self.__results[self.__index])).path)
+        self._set_entry(unwrap(self.__lib.get_entry(self.__results[self.__index])))
 
     @classmethod
     def build_modal(cls, driver: "QtDriver") -> PanelModal["QuickTaggingPanel"]:
--- src/tagstudio/qt/views/quick_tagging_panel_view.py
+++ src/tagstudio/qt/views/quick_tagging_panel_view.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from PySide6.QtWidgets import QHBoxLayout
+from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout
 
 from tagstudio.core.library.alchemy.library import Library
+from tagstudio.core.library.alchemy.models import Entry
 from tagstudio.qt.controllers.preview_thumb_controller import PreviewThumb
+from tagstudio.qt.mixed.field_containers import FieldContainers
+from tagstudio.qt.mixed.file_attributes import FileAttributes
 from tagstudio.qt.views.panel_modal import PanelWidget
 
 if TYPE_CHECKING:
@@ -12,20 +15,33 @@ if TYPE_CHECKING:
 
 
 class QuickTaggingPanelView(PanelWidget):
-    _lib: Library
+    __lib: Library
 
     def __init__(self, driver: "QtDriver"):
         super().__init__()
-        self._lib = driver.lib
+        self.__lib = driver.lib
 
         root_layout = QHBoxLayout(self)
         root_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.__preview_thumb = PreviewThumb(self._lib, driver)
+        left_panel_layout = QVBoxLayout(self)
+
+        self.__file_attrs = FileAttributes(self.__lib, driver)
+        left_panel_layout.addWidget(self.__file_attrs)
+
+        self.__fields = FieldContainers(self.__lib, driver)
+        left_panel_layout.addWidget(self.__fields)
+
+        root_layout.addLayout(left_panel_layout)
+
+        self.__preview_thumb = PreviewThumb(self.__lib, driver)
         root_layout.addWidget(self.__preview_thumb)
 
-    def _set_entry(self, entry_path: Path) -> None:
-        assert self._lib.library_dir is not None
-        filepath: Path = self._lib.library_dir / entry_path
+    def _set_entry(self, entry: Entry) -> None:
+        assert self.__lib.library_dir is not None
+        filepath: Path = self.__lib.library_dir / entry.path
 
-        self.__preview_thumb.display_file(filepath)
+        stats = self.__preview_thumb.display_file(filepath)
+
+        self.__file_attrs.update_stats(filepath, stats)
+        self.__fields.update_from_entry(entry.id, update_badges=False)
