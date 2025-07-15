5844c2e5220a090c0bd1eda317b1fc98cffa4582
Jann Stute <jann.stute@protonmail.com>
2025-07-15 22:56:15 +0200

    feat: add file attribute and field container widgets

--- src/tagstudio/qt/view/widgets/quick_tagging_panel_view.py
+++ src/tagstudio/qt/view/widgets/quick_tagging_panel_view.py
@@ -1,32 +1,47 @@
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from PySide6.QtWidgets import QHBoxLayout
+from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout
 
 from tagstudio.core.library.alchemy.library import Library
 from tagstudio.core.library.alchemy.models import Entry
 from tagstudio.qt.controller.widgets.preview.preview_thumb_controller import PreviewThumb
 from tagstudio.qt.widgets.panel import PanelWidget
+from tagstudio.qt.widgets.preview.field_containers import FieldContainers
+from tagstudio.qt.widgets.preview.file_attributes import FileAttributes
 
 if TYPE_CHECKING:
     from tagstudio.qt.ts_qt import QtDriver
 
 
 class QuickTaggingPanelView(PanelWidget):
-    _lib: Library
+    __lib: Library
 
     def __init__(self, driver: "QtDriver"):
         super().__init__()
-        self._lib = driver.lib
+        self.__lib = driver.lib
 
         root_layout = QHBoxLayout(self)
         root_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.__preview_thumb = PreviewThumb(self._lib, driver)
+        left_panel_layout = QVBoxLayout(self)
+
+        self.__file_attrs = FileAttributes(self.__lib, driver)
+        left_panel_layout.addWidget(self.__file_attrs)
+
+        self.__fields = FieldContainers(self.__lib, driver)
+        left_panel_layout.addWidget(self.__fields)
+
+        root_layout.addLayout(left_panel_layout)
+
+        self.__preview_thumb = PreviewThumb(self.__lib, driver)
         root_layout.addWidget(self.__preview_thumb)
 
     def _set_entry(self, entry: Entry) -> None:
-        assert self._lib.library_dir is not None
-        filepath: Path = self._lib.library_dir / entry.path
+        assert self.__lib.library_dir is not None
+        filepath: Path = self.__lib.library_dir / entry.path
 
-        self.__preview_thumb.display_file(filepath)
+        stats = self.__preview_thumb.display_file(filepath)
+
+        self.__file_attrs.update_stats(filepath, stats)
+        self.__fields.update_from_entry(entry.id, update_badges=False)
