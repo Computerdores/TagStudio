f349eba1e59a82e0515a40f1c01a8d838928868a
Jann Stute <jann.stute@protonmail.com>
2025-07-15 22:56:15 +0200

    feat: add file attribute and field container widgets

--- src/tagstudio/qt/controller/widgets/quick_tagging_panel_controller.py
+++ src/tagstudio/qt/controller/widgets/quick_tagging_panel_controller.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from tagstudio.core.library.alchemy.enums import BrowsingState
+from tagstudio.core.library.alchemy.library import Library
 from tagstudio.core.library.alchemy.models import Entry
 from tagstudio.qt.view.widgets.quick_tagging_panel_view import QuickTaggingPanelView
 from tagstudio.qt.widgets.panel import PanelModal
@@ -10,16 +11,18 @@ if TYPE_CHECKING:
 
 
 class QuickTaggingPanel(QuickTaggingPanelView):
+    __lib: Library
     __results: list[Entry]
     __index: int
 
     def __init__(self, driver: "QtDriver"):
         super().__init__(driver)
+        self.__lib = driver.lib
 
     def set_search(self, query: BrowsingState) -> None:
         self.__index = 0
 
-        self.__results = self._lib.search_library(query, None).items
+        self.__results = self.__lib.search_library(query, None).items
 
         self._set_entry(self.__results[self.__index])
 
