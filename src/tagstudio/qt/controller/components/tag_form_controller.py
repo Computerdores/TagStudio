50ed3dc28532d71654a3eca7aa7f45ee46ae1bdd
Jann Stute <jann.stute@protonmail.com>
2025-08-03 15:44:08 +0200

    feat: clicking tag adds to entry

--- src/tagstudio/qt/controller/components/tag_form_controller.py
+++ src/tagstudio/qt/controller/components/tag_form_controller.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
 
 from tagstudio.core.library.alchemy.library import Library
+from tagstudio.core.library.alchemy.models import Tag
 from tagstudio.qt.view.components.tag_form_view import TagForm, TagFormComponentView
 
 if TYPE_CHECKING:
@@ -9,7 +10,17 @@ if TYPE_CHECKING:
 
 class TagFormComponent(TagFormComponentView):
     __lib: Library
+    __current_entry: int
 
     def __init__(self, driver: "QtDriver", form: TagForm, parent=None):
         super().__init__(driver, form, parent)
         self.__lib = driver.lib
+
+    @override
+    def set_entry(self, entry: int):  # type: ignore[misc]
+        self.__current_entry = entry
+        return super().set_entry(entry)
+
+    def _on_tag_click(self, tag: Tag):
+        self.__lib.add_tags_to_entries(self.__current_entry, tag.id)
+        self.on_update.emit()
